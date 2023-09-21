639720901b54b9ad59234f4b964817b2865679f7
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-09-22 00:53:16 +0300

    install  commander

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import * as contactsService from "./contacts.js";
 import yargs from "yargs";
 
-const { argv } = yargs(process.argv);
-console.log(argv);
+const { argv } = yargs(process.argv.slice(2));
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
